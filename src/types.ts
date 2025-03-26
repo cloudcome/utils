7de75ee0280be64ce4a84d49afe49a2ee4251ae2
☁️淡然 <hi@ydr.me>
2025-03-26 14:32:24 +0800

    refactor(types): 添加通用的类型别名
    - 新增 AnyObject、AnyArray 和 AnyFunction 三种类型别名
    - 这些类型别名使用了 any 类型，方便在需要时忽略特定的类型检查
    - 在类型定义前添加了 biome-ignore 注释，用于忽略相关的 lint 警告

---
+++ src/types.ts
@@ -0,0 +1,8 @@
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+export type AnyObject = Record<string, any>;
+
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+export type AnyArray = any[];
+
+// biome-ignore lint/suspicious/noExplicitAny: <explanation>
+export type AnyFunction = (...args: any[]) => any;
