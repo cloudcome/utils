89a738bb27c3540db1393d15813781fa55df50fd
☁️淡然 <hi@ydr.me>
2025-03-29 14:29:23 +0800

    test(deep): 重构测试用例并增加遍历信息断言
    - 重构测试用例，使用接口 TestDeepItem 定义测试数据结构
    - 增加对遍历过程中 parent、level 和 path 信息的断言
    - 补充广度优先遍历的测试用例
    - 移除未使用的代码片段

--- test/deep.test.ts
+++ test/deep.test.ts
@@ -1,56 +1,182 @@
-import { deepEach } from '@/deep';
+import { type DeepItem, type DeepList, deepEach } from '@/deep';
 import { describe, expect, it } from 'vitest';
 
-describe('deepEach', () => {
-  const deepList = [{ value: 1, children: [{ value: 2 }, { value: 3 }] }, { value: 4 }];
+interface TestDeepItem extends DeepItem {
+  id: string;
+}
 
+const deepList: TestDeepItem[] = [
+  {
+    id: '1',
+    children: [
+      {
+        id: '1-1',
+        children: [
+          {
+            id: '1-1-1',
+          },
+        ],
+      },
+      {
+        id: '1-2',
+        children: [
+          {
+            id: '1-2-1',
+          },
+        ],
+      },
+    ],
+  },
+];
+
+describe('deepEach', () => {
   it('应正确执行深度优先遍历', () => {
-    const results: number[] = [];
+    const results: string[] = [];
 
     deepEach(deepList, (info) => {
-      results.push(info.item.value);
+      results.push(info.item.id);
     });
 
-    expect(results).toEqual([1, 2, 3, 4]);
+    expect(results).toEqual(['1', '1-1', '1-1-1', '1-2', '1-2-1']);
   });
 
   it('应正确执行广度优先遍历', () => {
-    const results: number[] = [];
+    const results: string[] = [];
 
     deepEach(
       deepList,
       (info) => {
-        results.push(info.item.value);
+        results.push(info.item.id);
       },
       true,
     );
 
-    expect(results).toEqual([1, 4, 2, 3]);
+    expect(results).toEqual(['1', '1-1', '1-2', '1-1-1', '1-2-1']);
   });
 
   it('应支持提前终止深度遍历', () => {
-    const results: number[] = [];
+    const results: string[] = [];
 
     deepEach(deepList, (info) => {
-      results.push(info.item.value);
-      if (info.item.value === 2) return false;
+      results.push(info.item.id);
+      if (info.item.id === '1-1-1') return false;
     });
 
-    expect(results).toEqual([1, 2]);
+    expect(results).toEqual(['1', '1-1', '1-1-1']);
   });
 
   it('应支持提前终止深度遍历', () => {
-    const results: number[] = [];
+    const results: string[] = [];
 
     deepEach(
       deepList,
       (info) => {
-        results.push(info.item.value);
-        if (info.item.value === 4) return false;
+        results.push(info.item.id);
+        if (info.item.id === '1-2') return false;
       },
       true,
     );
 
-    expect(results).toEqual([1, 4]);
+    expect(results).toEqual(['1', '1-1', '1-2']);
+  });
+
+  it('深度优先信息', () => {
+    const idList: string[] = [];
+    const parentList: (TestDeepItem | null)[] = [];
+    const levelList: number[] = [];
+    const pathList: string[][] = [];
+
+    deepEach(deepList, ({ item, index, list, parent, level, path }) => {
+      idList.push(item.id);
+      parentList.push(parent);
+      levelList.push(level);
+      pathList.push(path.map((item) => item.id));
+    });
+
+    let i = 0;
+    // console.log(idList);
+    expect(idList[i++]).toEqual('1');
+    expect(idList[i++]).toEqual('1-1');
+    expect(idList[i++]).toEqual('1-1-1');
+    expect(idList[i++]).toEqual('1-2');
+    expect(idList[i++]).toEqual('1-2-1');
+    expect(idList[i++]).toEqual(undefined);
+    let j = 0;
+    // console.log(parentList);
+    expect(parentList[j++]).toEqual(null);
+    expect(parentList[j++]?.id).toEqual('1');
+    expect(parentList[j++]?.id).toEqual('1-1');
+    expect(parentList[j++]?.id).toEqual('1');
+    expect(parentList[j++]?.id).toEqual('1-2');
+    expect(parentList[j++]).toEqual(undefined);
+    let k = 0;
+    // console.log(levelList);
+    expect(levelList[k++]).toEqual(1);
+    expect(levelList[k++]).toEqual(2);
+    expect(levelList[k++]).toEqual(3);
+    expect(levelList[k++]).toEqual(2);
+    expect(levelList[k++]).toEqual(3);
+    expect(levelList[k++]).toEqual(undefined);
+
+    expect(pathList).toEqual([
+      //
+      ['1'],
+      ['1', '1-1'],
+      ['1', '1-1', '1-1-1'],
+      ['1', '1-2'],
+      ['1', '1-2', '1-2-1'],
+    ]);
+  });
+
+  it('广度优先信息', () => {
+    const idList: string[] = [];
+    const parentList: (TestDeepItem | null)[] = [];
+    const levelList: number[] = [];
+    const pathList: string[][] = [];
+
+    deepEach(
+      deepList,
+      ({ item, index, list, parent, level, path }) => {
+        idList.push(item.id);
+        parentList.push(parent);
+        levelList.push(level);
+        pathList.push(path.map((item) => item.id));
+      },
+      true,
+    );
+
+    let i = 0;
+    // console.log(idList);
+    expect(idList[i++]).toEqual('1');
+    expect(idList[i++]).toEqual('1-1');
+    expect(idList[i++]).toEqual('1-2');
+    expect(idList[i++]).toEqual('1-1-1');
+    expect(idList[i++]).toEqual('1-2-1');
+    expect(idList[i++]).toEqual(undefined);
+    let j = 0;
+    // console.log(parentList);
+    expect(parentList[j++]).toEqual(null);
+    expect(parentList[j++]?.id).toEqual('1');
+    expect(parentList[j++]?.id).toEqual('1');
+    expect(parentList[j++]?.id).toEqual('1-1');
+    expect(parentList[j++]?.id).toEqual('1-2');
+    expect(parentList[j++]).toEqual(undefined);
+    let k = 0;
+    // console.log(levelList);
+    expect(levelList[k++]).toEqual(1);
+    expect(levelList[k++]).toEqual(2);
+    expect(levelList[k++]).toEqual(2);
+    expect(levelList[k++]).toEqual(3);
+    expect(levelList[k++]).toEqual(3);
+    expect(levelList[k++]).toEqual(undefined);
+
+    expect(pathList).toEqual([
+      //
+      ['1'],
+      ['1', '1-1'],
+      ['1', '1-2'],
+      ['1', '1-1', '1-1-1'],
+      ['1', '1-2', '1-2-1'],
+    ]);
   });
 });
