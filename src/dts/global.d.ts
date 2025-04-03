134a7983e8f8f17c51bfaeac9acafecb8e319f8f
☁️淡然 <hi@ydr.me>
2025-04-04 00:14:01 +0800

    feat(dts): 添加测试相关声明变量
    - 新增 IS_TEST声明，用于标识是否在测试环境中
    - 添加 TEST_MOCK 接口和 TEST_MOCK声明，用于模拟测试环境下的变量

--- src/dts/global.d.ts
+++ src/dts/global.d.ts
@@ -16,3 +16,12 @@ declare const PKG_VERSION: string;
  * package.json description
  */
 declare const PKG_DESCRIPTION: string;
+
+declare const IS_TEST: string;
+
+interface TEST_MOCK {
+  IS_BROWSER: boolean;
+  IS_NODE: boolean;
+}
+
+declare const TEST_MOCK: TEST_MOCK;
