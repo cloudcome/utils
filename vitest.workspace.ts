5ea8019ef142bf96f3fb4116d6f28c851c869b74
☁️淡然 <hi@ydr.me>
2025-04-07 10:42:05 +0800

    style(vitest.workspace.ts): 格式化 vitest.workspace.ts 文件
    - 添加缺失的分号
    - 统一使用单引号
    - 调整缩进和空格

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,12 +1,12 @@
-import { defineWorkspace } from 'vitest/config'
+import { defineWorkspace } from 'vitest/config';
 
 export default defineWorkspace([
-  "./packages/browser-utils/vite.config.mts",
-  "./packages/core-utils/vite.config.mts",
-  "./packages/node-utils/vite.config.mts",
-  "./packages/uni-utils/vite.config.mts",
-  "./packages/react-hooks/vite.config.mts",
-  "./packages/uni-hooks/vite.config.mts",
-  "./packages/uni-utils/vite.config.mts",
-  "./packages/vue-composables/vite.config.mts"
-])
+  './packages/browser-utils/vite.config.mts',
+  './packages/core-utils/vite.config.mts',
+  './packages/node-utils/vite.config.mts',
+  './packages/uni-utils/vite.config.mts',
+  './packages/react-hooks/vite.config.mts',
+  './packages/uni-hooks/vite.config.mts',
+  './packages/uni-utils/vite.config.mts',
+  './packages/vue-composables/vite.config.mts',
+]);
