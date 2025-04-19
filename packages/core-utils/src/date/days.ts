50e51dc94100e5976be0f5497ef5bf71838ea209
☁️淡然 <hi@ydr.me>
2025-04-19 14:53:25 +0800

    feat(core-utils): 添加计算指定日期天数的函数
    - 新增 dateDays、dateDaysInMonth 和 dateDaysInYear 函数
    - 可以计算指定日期所在年或月的天数
    - 支持数值、字符串或 Date 对象作为输入
    - 闰年和非闰年情况均适用

---
+++ packages/core-utils/src/date/days.ts
@@ -0,0 +1,31 @@
+import { DATE_DAY_MS } from './const';
+import { type DateValue, dateParse } from './core';
+import { dateEndInMonth, dateEndInYear, dateStartInMonth, dateStartInYear } from './start-end';
+
+/**
+ * 计算指定日期所在年或月的天数
+ * @param dateValue - 可以是数值、字符串或 Date 对象
+ * @param unit - 时间单位符号，可选值为 'Y'（年）、'M'（月），默认为 'M'
+ * @returns 返回指定日期所在年或月的天数
+ * @example
+ * ```typescript
+ * dateDays(new Date('2023-02-15')); // 28
+ * dateDays(new Date('2024-02-15')); // 29 (闰年)
+ * dateDays(new Date('2023-02-15'), 'Y'); // 365
+ * dateDays(new Date('2024-02-15'), 'Y'); // 366 (闰年)
+ * ```
+ */
+function _dateDays(dateValue: DateValue, unit: 'Y' | 'M') {
+  const d = dateParse(dateValue);
+  const ds = unit === 'M' ? dateStartInMonth(d) : dateStartInYear(d);
+  const de = unit === 'M' ? dateEndInMonth(d) : dateEndInYear(d);
+  return Math.ceil((de.getTime() - ds.getTime()) / DATE_DAY_MS);
+}
+
+export function dateDaysInMonth(dateValue: DateValue) {
+  return _dateDays(dateValue, 'M');
+}
+
+export function dateDaysInYear(dateValue: DateValue) {
+  return _dateDays(dateValue, 'Y');
+}
