8959bb344443846affc3858541b8062d5b613e58
☁️淡然 <hi@ydr.me>
2025-04-19 13:42:19 +0800

    feat(core-utils): 添加日期常量
    - 新增日期相关的常量，用于简化日期计算和转换
    - 包括秒、分、时、天、月、年等时间单位的毫秒值

---
+++ packages/core-utils/src/date/const.ts
@@ -0,0 +1,6 @@
+export const DATE_SECOND_MS = 1000;
+export const DATE_MINUTE_MS = 60 * DATE_SECOND_MS;
+export const DATE_HOUR_MS = 60 * DATE_MINUTE_MS;
+export const DATE_DAY_MS = 24 * DATE_HOUR_MS;
+export const DATE_MONTH_MS = 30 * DATE_DAY_MS;
+export const DATE_YEAR_MS = 365 * DATE_DAY_MS;
