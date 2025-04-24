9f2a97d4b6df9c6951ed346974dbaf797a1de0b1
☁️淡然 <hi@ydr.me>
2025-04-24 20:28:01 +0800

    refactor(core-utils): 优化 isObject 函数定义
    - 移除函数参数的泛型类型 T
    - 移除函数返回值的泛型类型 T
    - 这些泛型类型在函数实现中并未使用，移除后可提高代码的可读性和性能

--- packages/core-utils/src/type.ts
+++ packages/core-utils/src/type.ts
@@ -131,7 +131,7 @@ export function isPrimitive(
  * @param unknown - 未知类型的值
  * @returns 如果值为对象则返回 true，否则返回 false
  */
-export function isObject<T>(unknown: T): unknown is AnyObject<T> {
+export function isObject(unknown: unknown): unknown is AnyObject {
   return typeIs(unknown) === 'object';
 }
 
