7fd24411e357aa6cbfb9e0ca9961d4fcc3cde6e7
☁️淡然 <hi@ydr.me>
2025-04-26 00:18:01 +0800

    feat(utils-browser): 添加下载功能
    - 实现了 downloadURL 和 downloadBlob 两个函数
    - 添加了下载功能的单元测试

---
+++ packages/utils-browser/src/download.ts
@@ -0,0 +1,22 @@
+/**
+ * 下载 URL
+ * @param {string} url
+ * @param {string} filename
+ */
+export function downloadURL(url: string, filename?: string) {
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = filename || '';
+  a.click();
+}
+
+/**
+ * 下载 Blob 对象
+ * @param blob Blob 对象
+ * @param filename 文件名
+ */
+export function downloadBlob(blob: Blob, filename?: string) {
+  const url = URL.createObjectURL(blob);
+  downloadURL(url, filename);
+  URL.revokeObjectURL(url);
+}
---
+++ packages/utils-browser/test/download.test.ts
@@ -0,0 +1,15 @@
+import { downloadBlob, downloadURL } from '@/download';
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
+
+it('download', () => {
+  downloadURL('/');
+  downloadURL('/', 'file');
+
+  URL.createObjectURL = vi.fn(() => 'url');
+  URL.revokeObjectURL = vi.fn();
+
+  downloadBlob(new Blob());
+  downloadBlob(new Blob(), 'file');
+
+  expect(URL.createObjectURL).toBeTypeOf('function');
+});
