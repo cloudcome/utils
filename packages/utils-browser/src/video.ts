d4ef7a59a379156dbba8737d18582288ae306900
☁️淡然 <hi@ydr.me>
2025-04-28 11:23:18 +0800

    feat(utils-browser): 添加视频加载功能
    - 实现了一个名为 videoLoad 的函数，用于加载视频并返回 HTMLVideoElement
    - 函数接受一个视频 URL 作为参数，返回一个 Promise 对象
    - 在视频加载成功时，Promise 会解析为包含视频元素的 Promise
    - 如果视频加载失败，会抛出错误
    - 此功能扩展了 utils-browser 包的功能范围

---
+++ packages/utils-browser/src/video.ts
@@ -0,0 +1,19 @@
+/**
+ * 加载视频并返回一个包含 HTMLVideoElement 的 Promise
+ * @param {string} url - 视频的 URL 地址
+ * @returns {Promise<HTMLVideoElement>} 返回一个包含 HTMLVideoElement 的 Promise
+ * @example
+ * const video = await videoLoad('https://example.com/video.mp4');
+ * @throws {Error} 如果视频加载失败，抛出错误
+ */
+export async function videoLoad(url: string) {
+  return new Promise<HTMLVideoElement>((resolve, reject) => {
+    const video = document.createElement('video');
+
+    video.src = url;
+    video.crossOrigin = 'anonymous';
+    video.currentTime = 1;
+    video.onloadedmetadata = () => resolve(video);
+    video.onerror = () => reject(new Error('视频加载失败'));
+  });
+}
