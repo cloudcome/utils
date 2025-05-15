978b424cade6d0caa1c5b8876ca3ca56e0fd95be
☁️淡然 <hi@ydr.me>
2025-05-15 19:38:32 +0800

    refactor(utils-browser): 更新定时器类型定义
    - 将 ITimerHandler、ITimerOptions 和 ITimerState 类型的命名从 I 开头改为 T 开头
    - 更新 frameInterval 函数的参数和返回值类型定义

--- packages/utils-browser/src/timer.ts
+++ packages/utils-browser/src/timer.ts
@@ -1,16 +1,16 @@
-import { type ITimerHandler, type ITimerOptions, type ITimerState, makeInterval } from '@cloudcome/utils-core/timer';
+import { type TTimerHandler, type TTimerOptions, type TTimerState, makeInterval } from '@cloudcome/utils-core/timer';
 
 /**
  * 创建一个基于 `requestAnimationFrame` 的间隔定时器
  *
  * @param callback - 每次间隔执行的回调函数，接收定时器状态和可选的 `next` 函数
  * @param options - 配置选项
- * @returns {ITimerHandler} 返回包含控制方法的对象
+ * @returns {TTimerHandler} 返回包含控制方法的对象
  */
 export function frameInterval(
-  callback: (state: ITimerState, next?: () => void) => unknown,
-  options?: ITimerOptions,
-): ITimerHandler {
+  callback: (state: TTimerState, next?: () => void) => unknown,
+  options?: TTimerOptions,
+): TTimerHandler {
   let rafId: number;
   const { canStart, start, canStop, stop, canPause, pause, canResume, resume, execute } = makeInterval((call) => {
     rafId = requestAnimationFrame(call);
