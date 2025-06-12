1b46a3b0ec3300c69c954049f03c4acd0f75c09d
☁️淡然 <hi@ydr.me>
2025-06-12 10:55:55 +0800

    feat(utils-core): 添加自定义异常构建函数
    - 实现了 buildException 函数，用于创建自定义异常类
    - 支持传入异常名称和可选的格式化函数
    - 新增异常类继承自 Error，可携带额外的自定义属性
    - 提供了使用示例，展示了如何创建和使用自定义异常

---
+++ packages/utils-core/src/exception.ts
@@ -0,0 +1,33 @@
+import { objectDefaults } from './object';
+import type { AnyObject } from './types';
+
+export type BuildExceptionOptions = {
+  format?: (name: string, message: string) => string;
+};
+
+const defaults: BuildExceptionOptions = {
+  format: (name, message) => `[${name}] ${message}`,
+};
+
+export function buildException<T extends AnyObject>(name: string, options?: BuildExceptionOptions) {
+  const { format } = objectDefaults(options || {}, defaults) as Required<BuildExceptionOptions>;
+
+  return class extends Error {
+    constructor(message: string, extra: T) {
+      super(format(name, message), extra);
+      this.name = name;
+      Object.assign(this, extra);
+    }
+  } as unknown as {
+    new (message: string, extra: T): Error & T;
+  };
+}
+
+// const MyException = buildException<{ foo: string; bar: number }>('MyException: ');
+// const myException = new MyException('bar', { foo: '1', bar: 1 });
+// myException.foo;
+// myException.bar;
+// myException.name;
+// myException.message;
+// myException.stack;
+// myException.cause;
