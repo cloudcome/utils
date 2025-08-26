54633c7f057d099693dab617fccc146a69d6d794
☁️淡然 <hi@ydr.me>
2025-08-27 06:12:32 +0800

    ci: 更新版本管理脚本
    - 优化了版本号输出格式
    - 移除了未使用的命令变量
    - 替换 console.log 为 echo 输出命令
    - 直接使用 zx 的 $ 执行命令

--- scripts/ci-version.ts
+++ scripts/ci-version.ts
@@ -1,13 +1,11 @@
 #!/usr/bin/env zx
 
-import { $ } from 'zx';
+import { $, echo } from 'zx';
 import pkg from '../package.json';
 
 const firstVersion = process.argv.slice(2)[0] === '--first-version=true';
-const bump = firstVersion ? pkg.version : '';
+const bump = firstVersion ? ` ${pkg.version}` : '';
 
 // https://github.com/lerna/lerna/tree/main/libs/commands/version
-// const command = `npx lerna version ${bump} --yes --create-release=github`;
-const command = `npx lerna version ${bump} --yes --no-push`;
-console.log(command);
-await $`${command}`;
+echo(`npx lerna version${bump} --yes --create-release=github`);
+await $`npx lerna version${bump} --yes --create-release=github`;
