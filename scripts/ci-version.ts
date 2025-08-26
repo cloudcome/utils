14ccec2e9659926b6b1459c5dbd953b71c857bf3
☁️淡然 <hi@ydr.me>
2025-08-27 06:23:00 +0800

    ci: 更新版本管理脚本
    - 移除了对 '--first-version=true' 参数的冗余空格
    - 重新启用了创建 GitHub release 的功能
    - 优化了代码格式，包括空格和换行

--- scripts/ci-version.ts
+++ scripts/ci-version.ts
@@ -2,13 +2,12 @@ import { execSync } from 'node:child_process';
 import pkg from '../package.json';
 
 const firstVersion = process.argv.slice(2)[0] === '--first-version=true';
-const bump = firstVersion ? ` ${pkg.version}` : '';
+const bump = firstVersion ? `${pkg.version}` : '';
 
 // https://github.com/lerna/lerna/tree/main/libs/commands/version
-// await $`npx lerna version${bump} --yes --create-release=github`;
-run(`npx lerna version${bump} --yes --no-push`);
+run(`npx lerna version ${bump} --yes --create-release=github`);
 
 function run(command: string) {
-  console.log(command);
+  console.log('>', command);
   execSync(command, { stdio: 'inherit' });
 }
