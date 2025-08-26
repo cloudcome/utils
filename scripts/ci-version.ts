a321fae589a4fef01a161aca4410e154e7bec5eb
☁️淡然 <hi@ydr.me>
2025-08-27 06:06:36 +0800

    build(ci): 更新版本发布脚本
    - 注释掉原有的命令行，使用新的命令进行版本发布
    - 新增 --no-push 参数，避免在发布版本时自动推送代码

--- scripts/ci-version.ts
+++ scripts/ci-version.ts
@@ -7,6 +7,7 @@ const firstVersion = process.argv.slice(2)[0] === '--first-version=true';
 const bump = firstVersion ? pkg.version : '';
 
 // https://github.com/lerna/lerna/tree/main/libs/commands/version
-const command = `npx lerna version ${bump} --yes --create-release=github`;
+// const command = `npx lerna version ${bump} --yes --create-release=github`;
+const command = `npx lerna version ${bump} --yes --no-push`;
 console.log(command);
 await $`${command}`;
