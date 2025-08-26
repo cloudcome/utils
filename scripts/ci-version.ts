358430bb8a9ab2acc37bc882cd6b7660383942c6
☁️淡然 <hi@ydr.me>
2025-08-27 06:19:53 +0800

    ci: 更新版本脚本
    - 用 execSync 替代 zx 执行命令
    - 移除 echo 和 $ 的引用
    - 添加 run 函数来执行命令
    - 更新文件头部注释

--- scripts/ci-version.ts
+++ scripts/ci-version.ts
@@ -1,12 +1,14 @@
-#!/usr/bin/env zx
-
-import { $, echo } from 'zx';
+import { execSync } from 'node:child_process';
 import pkg from '../package.json';
 
 const firstVersion = process.argv.slice(2)[0] === '--first-version=true';
 const bump = firstVersion ? ` ${pkg.version}` : '';
 
 // https://github.com/lerna/lerna/tree/main/libs/commands/version
-echo(`npx lerna version${bump} --yes --create-release=github`);
 // await $`npx lerna version${bump} --yes --create-release=github`;
-await $`npx lerna version${bump} --yes --no-push`;
+run(`npx lerna version${bump} --yes --no-push`);
+
+function run(command: string) {
+  console.log(command);
+  execSync(command, { stdio: 'inherit' });
+}
