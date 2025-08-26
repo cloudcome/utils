3f400492c9c08f7d9935b4e47dcc540d49c5b45c
☁️淡然 <hi@ydr.me>
2025-08-27 06:01:47 +0800

    ci: 打印版本控制命令
    在 CI 版本控制脚本中添加了打印命令的功能，以便于调试和验证。

--- scripts/ci-version.ts
+++ scripts/ci-version.ts
@@ -7,4 +7,6 @@ const firstVersion = process.argv.slice(2)[0] === '--first-version=true';
 const bump = firstVersion ? pkg.version : '';
 
 // https://github.com/lerna/lerna/tree/main/libs/commands/version
-await $`npx lerna version ${bump} --yes --create-release=github`;
+const command = `npx lerna version ${bump} --yes --create-release=github`;
+console.log(command);
+await $`${command}`;
