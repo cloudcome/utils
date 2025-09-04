95edb6c589b241ae94b8925a0684e8b1e83b4e55
☁️淡然 <hi@ydr.me>
2025-09-04 22:57:53 +0800

    fix(utils-uni): 重命名云对象和数据库返回类型
    - 将 UniCloudObjectReturns<T> 重命名为 UniCloudObjectOutput<T>
    - 将 UniCloudDatabaseReturns<T> 重命名为 UniCloudDatabaseOutput<T>
    - 更新了相关函数和类型的引用

--- packages/utils-uni/src/cloud.ts
+++ packages/utils-uni/src/cloud.ts
@@ -4,13 +4,13 @@ import { type UseRequestOptions, useRequest } from '@cloudcome/utils-vue/request
 type ImportObject = UniCloudNamespace.UniCloud['importObject'];
 type ImportObjectArgs = Parameters<ImportObject>;
 
-export type UniCloudObjectReturns<T> = {
+export type UniCloudObjectOutput<T> = {
   errCode?: number | string;
   errMsg?: string;
   data: T;
 };
 
-type UniCloudObjectServer<I extends AnyArray, T> = (...inputs: I) => Promise<UniCloudObjectReturns<T>>;
+type UniCloudObjectServer<I extends AnyArray, T> = (...inputs: I) => Promise<UniCloudObjectOutput<T>>;
 
 export type CreateUseCloudObjectOptions = ImportObjectArgs[1] & {
   /**
@@ -36,7 +36,7 @@ export function createUseCloudObject(objectName: ImportObjectArgs[0], options?: 
    * @returns 返回一个请求hook，用于处理云对象调用
    */
   return function useCloudObject<I extends AnyArray, O>(
-    caller: (server: UniCloudObjectServer<I, O>, ...inputs: I) => Promise<UniCloudObjectReturns<O>>,
+    caller: (server: UniCloudObjectServer<I, O>, ...inputs: I) => Promise<UniCloudObjectOutput<O>>,
     options?: UseRequestOptions<I, O>,
   ) {
     // 使用请求hook处理云对象调用
@@ -48,7 +48,7 @@ export function createUseCloudObject(objectName: ImportObjectArgs[0], options?: 
   };
 }
 
-export type UniCloudDatabaseReturns<T> = {
+export type UniCloudDatabaseOutput<T> = {
   result: T & {
     errCode?: number | string;
     errMsg?: string;
@@ -70,7 +70,7 @@ export type UseCloudDatabaseOptions<I extends AnyArray, O> = UseRequestOptions<I
  * @returns 返回一个请求hook，用于处理云数据库调用
  */
 export function useCloudDatabase<I extends AnyArray, O>(
-  caller: (db: UniCloud.Database, ...inputs: I) => Promise<UniCloudDatabaseReturns<O>>,
+  caller: (db: UniCloud.Database, ...inputs: I) => Promise<UniCloudDatabaseOutput<O>>,
   options?: UseCloudDatabaseOptions<I, O>,
 ) {
   // 获取数据库实例，优先使用模拟数据库（用于测试），否则使用uniCloud数据库
