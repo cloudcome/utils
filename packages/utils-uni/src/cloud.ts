37483a01ecd5b72e5329ad8ec8d5a9e737e7d2ea
☁️淡然 <hi@ydr.me>
2025-09-13 04:16:34 +0800

    refactor(utils-uni): 重构云对象相关类型定义并优化代码结构
    - 更新了 UniCloudObjectServer 类型定义，使其更通用
    - 新增 UseCloudObject 类型，用于定义云对象调用 Hook 的函数
    - 重构 createUseCloudObject 函数，使用新增的 UseCloudObject 类型
    - 优化了代码格式，如引号、换行等

--- packages/utils-uni/src/cloud.ts
+++ packages/utils-uni/src/cloud.ts
@@ -1,5 +1,10 @@
 import type { AnyArray } from '@cloudcome/utils-core/types';
-import { type UseRequestOptions, useRequest } from '@cloudcome/utils-vue/request';
+import {
+  type UseRequestOptions,
+  type UseRequestOutput,
+  type UseRequestOutputFilled,
+  useRequest,
+} from '@cloudcome/utils-vue/request';
 
 type ImportObject = UniCloudNamespace.UniCloud['importObject'];
 type ImportObjectArgs = Parameters<ImportObject>;
@@ -10,7 +15,7 @@ export type UniCloudObjectOutput<T> = {
   data: T;
 };
 
-type UniCloudObjectServer<I extends AnyArray, T> = (...inputs: I) => Promise<UniCloudObjectOutput<T>>;
+export type UniCloudObjectServer<O> = Record<string, (...inputs: unknown[]) => Promise<UniCloudObjectOutput<O>>>;
 
 export type CreateUseCloudObjectOptions = ImportObjectArgs[1] & {
   /**
@@ -21,6 +26,17 @@ export type CreateUseCloudObjectOptions = ImportObjectArgs[1] & {
   _mockServer?: any;
 };
 
+export type UseCloudObject = {
+  <I extends AnyArray, O>(
+    caller: (server: UniCloudObjectServer<O>, ...inputs: I) => Promise<UniCloudObjectOutput<O>>,
+    options: Omit<UseRequestOptions<I, O>, 'placeholder'> & { placeholder: () => O },
+  ): UseRequestOutputFilled<I, O>;
+  <I extends AnyArray, O>(
+    caller: (server: UniCloudObjectServer<O>, ...inputs: I) => Promise<UniCloudObjectOutput<O>>,
+    options?: UseRequestOptions<I, O>,
+  ): UseRequestOutput<I, O>;
+};
+
 /**
  * 创建一个用于调用云对象的hook工厂函数
  * @param objectName 云对象名称，用于导入对应的云对象
@@ -29,23 +45,16 @@ export type CreateUseCloudObjectOptions = ImportObjectArgs[1] & {
  */
 export function createUseCloudObject(objectName: ImportObjectArgs[0], options?: CreateUseCloudObjectOptions) {
   const server = options?._mockServer || uniCloud.importObject(objectName, options);
-
-  /**
-   * 创建云对象调用hook的函数
-   * @param caller 调用云对象的函数，接收云对象服务和输入参数，返回Promise
-   * @returns 返回一个请求hook，用于处理云对象调用
-   */
-  return function useCloudObject<I extends AnyArray, O>(
-    caller: (server: UniCloudObjectServer<I, O>, ...inputs: I) => Promise<UniCloudObjectOutput<O>>,
-    options?: UseRequestOptions<I, O>,
-  ) {
+  const useCloudObject: UseCloudObject = (caller, options) => {
     // 使用请求hook处理云对象调用
-    return useRequest(async (...inputs: I) => {
+    return useRequest(async (...inputs) => {
       const result = await caller(server, ...inputs);
       if (!result.errCode) return result.data;
       throw new Error(result.errMsg || '请求失败');
     }, options);
   };
+
+  return useCloudObject;
 }
 
 export type UniCloudDatabaseOutput<T> = {
