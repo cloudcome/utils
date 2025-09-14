061753b9b3a1827381a10df239865d14d3f80086
☁️淡然 <hi@ydr.me>
2025-09-14 16:56:49 +0800

    refactor(utils-core): 重构查询字符串相关代码
    - 将 querystring 相关代码重命名为 query
    - 更新 package.json 中的导出路径
    - 修改测试文件名称以更好地反映其测试内容
    - 更新 Vite 配置文件中的库入口路径

--- packages/utils-core/test/query.test.ts
+++ packages/utils-core/test/query.test.ts
@@ -1,5 +1,5 @@
 import { dateFormat } from '@/date';
-import { type QSReader, type QSWriter, qsParse, qsStringify } from '@/qs';
+import { type QSReader, type QSWriter, qsParse, qsStringify } from '@/query';
 import { isArray, isBoolean, isDate, isFunction, isNull, isNumber, isString, isUndefined } from '@/type';
 import { describe, expect, it } from 'vitest';
 
