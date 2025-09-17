f530f8d6bf961bef43e6d5029c78ea1f45bf6df4
☁️淡然 <hi@ydr.me>
2025-09-18 00:03:02 +0800

    test(utils-uni): 添加数据库查询和事务处理的测试代码
    - 新增了多个数据库查询示例，展示了如何使用 db 模块进行复杂的数据关联查询
    - 添加了一个事务处理示例，展示了如何使用 dbTransaction 函数执行事务操作
    - 通过类型断言和属性访问，验证了查询结果的类型和结构是否符合预期

---
+++ packages/utils-uni/src/database/test.ts
@@ -0,0 +1,146 @@
+import { db } from './db';
+import { dbTransaction } from './fns';
+
+type User = {
+  _id: string;
+  name: string;
+  age: number;
+  sex: 'male' | 'female';
+};
+type UserProfile = {
+  _id: string;
+  userId: string;
+  avatar: string;
+  bio: string;
+};
+type Post = {
+  _id: string;
+  title: string;
+  content: string;
+  authorId: string;
+  tagId: string[];
+};
+type Tag = {
+  _id: string;
+  name: string;
+  createdAt: number;
+  updatedAt: number;
+};
+type Comment = {
+  _id: string;
+  postId: string;
+  authorId: string;
+  content: string;
+  createdAt: number;
+  updatedAt: number;
+  likes: number;
+};
+
+const user1 = await db.table<User>('users').select({ age: true }).select({ age: true, sex: true }).queryOne();
+user1.age.toFixed();
+user1.sex.toLowerCase();
+assertType<{
+  _id: string;
+  age: number;
+  sex: 'male' | 'female';
+}>(user1);
+
+const userTable = db.table<User>('user');
+const userProfile = db.table<UserProfile>('profile');
+const postTable = db.table<Post>('posts');
+const tagTable = db.table<Tag>('tag');
+const commentTable = db.table<Comment>('comment');
+const user = await userTable
+  .lookup(
+    postTable
+      .select({ title: true })
+      .lookup(
+        commentTable.select({
+          content: true,
+          likes: true,
+        }),
+        {
+          as: 'comments',
+          type: '1:n',
+          localField: '_id',
+          foreignField: 'postId',
+        },
+      )
+      .lookup(
+        tagTable.select({
+          name: true,
+          createdAt: true,
+        }),
+        {
+          as: 'tags',
+          type: 'n:1',
+          localField: 'tagId',
+          foreignField: '_id',
+        },
+      ),
+    {
+      type: '1:n',
+      localField: '_id',
+      foreignField: 'authorId',
+      as: 'postList',
+    },
+  )
+  .lookup(userProfile.select({ avatar: true, bio: true }), {
+    type: '1:1',
+    localField: '_id',
+    foreignField: 'userId',
+    as: 'profile',
+  })
+  .queryOne();
+
+assertType<{
+  _id: string;
+  name: string;
+  age: number;
+  sex: 'male' | 'female';
+  postList: {
+    _id: string;
+    title: string;
+    comments: {
+      _id: string;
+      content: string;
+      likes: number;
+    }[];
+    tags: {
+      _id: string;
+      name: string;
+    }[];
+  }[];
+  profile: {
+    _id: string;
+    avatar: string;
+    bio: string;
+  };
+}>(user);
+
+user._id.length;
+user.name.length;
+user.age.toFixed();
+user.sex.toLowerCase();
+user.postList[0]._id.length;
+user.postList[0].title.length;
+user.postList[0].comments[0].content.length;
+user.postList[0].comments[0].likes.toFixed();
+user.postList[0].tags[0].name.length;
+user.postList[0].tags[0].createdAt.toFixed();
+user.profile.avatar.length;
+user.profile.bio.length;
+
+const result = await dbTransaction(async (wt) => {
+  const user = await wt(userTable).select({}).queryOne();
+  user.age.toFixed();
+  const post = await postTable.select({}).queryOne();
+  post.title.length;
+
+  return {
+    user,
+    post,
+  };
+});
+result.user.age.toFixed();
+result.post.title.length;
