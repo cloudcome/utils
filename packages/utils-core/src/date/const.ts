0a8594cd7db27e507c7948512937d8b4dd679ab8
☁️淡然 <hi@ydr.me>
2025-09-30 15:58:20 +0800

    feat(utils-core): 添加日期时间单位常量定义
    新增了常用的日期时间单位常量，包括秒、分钟、小时、天、月和年的毫秒数
    常量，方便在项目中统一使用和计算时间间隔。

--- packages/utils-core/src/date/const.ts
+++ packages/utils-core/src/date/const.ts
@@ -1,6 +1,29 @@
+/**
+ * 一秒的毫秒数
+ */
 export const DATE_SECOND_MS = 1000;
+
+/**
+ * 一分钟的毫秒数
+ */
 export const DATE_MINUTE_MS = 60 * DATE_SECOND_MS;
+
+/**
+ * 一小时的毫秒数
+ */
 export const DATE_HOUR_MS = 60 * DATE_MINUTE_MS;
+
+/**
+ * 一天的毫秒数
+ */
 export const DATE_DAY_MS = 24 * DATE_HOUR_MS;
+
+/**
+ * 一个月（30天）的毫秒数
+ */
 export const DATE_MONTH_MS = 30 * DATE_DAY_MS;
+
+/**
+ * 一年（365天）的毫秒数
+ */
 export const DATE_YEAR_MS = 365 * DATE_DAY_MS;
