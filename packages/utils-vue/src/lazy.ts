168970dc41514c64bac86213046e65aeac42273f
☁️淡然 <hi@ydr.me>
2025-10-10 18:39:44 +0800

    refactor(hooks): 重命名 useLazyValue 函数以更准确地反映其行为

--- packages/utils-vue/src/lazy.ts
+++ packages/utils-vue/src/lazy.ts
@@ -7,27 +7,27 @@ import { type Ref, computed, effectScope, onScopeDispose, ref, toValue, watch } 
  * 延迟值会在指定的延迟时间后更新。如果在延迟期间原始值再次变化，
  * 则会重新计算延迟时间。
  *
- * @param boolean 原始响应式值，类型为Ref<T>
+ * @param initialValue 原始响应式值，类型为Ref<T>
  * @param delay 延迟时间，可以是数字或响应式数字，默认为100毫秒
  * @returns 返回一个计算属性，其值会在延迟后更新为原始值
  */
-export function useLazyValue<T>(boolean: Ref<T>, delay?: number | Ref<number>) {
+export function useLazyValue<T>(initialValue: Ref<T>, delay?: number | Ref<number>) {
   const scope = effectScope();
-  const lazyBoolean = ref(boolean.value);
+  const lazyValue = ref(initialValue.value);
   let changedAt = 0;
   // biome-ignore lint/suspicious/noExplicitAny: <explanation>
   let t: any;
 
   scope.run(() => {
-    watch(boolean, (value) => {
+    watch(initialValue, (value) => {
       const delayValue = toValue(delay) || 100;
       clearTimeout(t);
 
       if (changedAt > 0 && Date.now() - changedAt > delayValue) {
-        lazyBoolean.value = value;
+        lazyValue.value = value;
       } else {
         t = setTimeout(() => {
-          lazyBoolean.value = value;
+          lazyValue.value = value;
         }, delayValue);
       }
 
@@ -41,6 +41,6 @@ export function useLazyValue<T>(boolean: Ref<T>, delay?: number | Ref<number>) {
   });
 
   return computed<T>(() => {
-    return lazyBoolean.value;
+    return lazyValue.value;
   });
 }
